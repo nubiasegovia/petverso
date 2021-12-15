b47b8134155bd008290558ee37882cb171b1f99c
Steffany Sympson <ssy.marques@gmail.com>
2021-12-14 22:01:18 -0300

    Cadastro PF

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes ,Route } from 'react-router-dom';
 import Navbar from './components/shared/Navbar/Navbar';
 import Footer from './components/shared/Footer/Footer';
 import Home from './components/pages/Home/Home';
+import CadastroUser from './components/pages/Cadastros/Usuario';
 
 
 
@@ -13,6 +14,7 @@ const App = () =>{
       <Navbar />
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/cadastro" element={<CadastroUser/>} />
       </Routes>
       <Footer />
     </div>
