5146793819369836849eada0ddff8e04695fc180
Núbia Segóvia <rsegovia.nubia@gmail.com>
2021-12-17 00:19:52 -0300

    [UPDATE] structure of images

--- src/components/pages/Products/ViewOneProduct.js
+++ src/components/pages/Products/ViewOneProduct.js
@@ -1,32 +1,44 @@
 import "./Products.css";
+import Arranhador1 from "../../assets/arranhador1.jpg";
+import Arranhador2 from "../../assets/arranhador2.jpg";
 
 
 const Product = () => {
   /* const Test = {
-        img1: "",
-        img2: "",
-        img3: "",
         titulo: "",
         empresa: "",
     } */
 
   return (
     <div className="container">
-      <div className="wrapper">
-        <div className="row align-items-start">
-          <div className="col imgs">
-            <div className="row1">Linha 1</div>
-            <div className="row2">Linha 2</div>
-            <div className="row3">Linha 3</div>
-          </div>
-          <div className="col img">
-            <div className="row selected-img">
-                Imagem-selecionada
+      <div className="product-container">
+        <div className="imgs-wrapper">
+          <div className="images-container">
+            <div className="imgs-col">
+              <div className="row1">
+                <img src={Arranhador1} alt="" />
+              </div>
+              <div className="row2">
+              <img src={Arranhador2} alt="" />
+              </div>
+              <div className="row3">
+                <img src={Arranhador2} alt="" />
+              </div>
+            </div>
+            <div className="col img">
+              <div className="row selected-img">
+                <img src={Arranhador1} alt="" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="infos-wrapper">
+          <p>Teste</p>
+        </div>
     </div>
+
+    </div>
+    
   );
 };
 
