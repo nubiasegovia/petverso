711496e5bbf56103042725aa15ca887dab80b5c0
Núbia Segóvia <rsegovia.nubia@gmail.com>
2022-01-02 23:59:59 -0300

    [UPDATE] link to products on nav

--- src/components/shared/Navbar/Navbar.js
+++ src/components/shared/Navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
                 <a href="/"> <img  className="logo-topo" src={PetLogo}/></a>
                 {/* <a href="/"  className="brand">Pet<b>Verso</b></a>   */}
                 <div className="menus">
-                    <a href="/products" className='menu1'> Produtos </a>
+                    <a href="/product/all" className='menu1'> Produtos </a>
                     <a href="/iniciativa"className='menu2'> Nossa Iniciativa </a>
                     <a target="_blank" href="#"className='menu3'> PetDicas </a>
                     <a href="/Login"><button className='btn-topo'>Login</button></a>
