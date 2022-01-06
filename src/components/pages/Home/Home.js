0da32d2392053fef91a936b5d7f5ea9469e662ca
Steffany Sympson <ssy.marques@gmail.com>
2022-01-06 19:00:11 -0300

    Ajustes de Home

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -2,14 +2,42 @@ import React from 'react';
 import './Home.css';
 import Carousel from './Carousel';
 import ProductList from '../../pages/ProductList/ProductList';
+import SilDog from '../../assets/sildog.png';
+import SilCat from '../../assets/silcat.png';
+import SilBird from '../../assets/silbird.png';
+import SilFish from '../../assets/silfish.png';
+import SilHabit from '../../assets/silhabit.png';
+import SilRep from '../../assets/silrep.png';
+import SilMouse from '../../assets/silmouse.png';
+import HomeText from '../../assets/hometext.png';
 
 
 
 const Home = () => {
     return (
         <>
-           <Carousel/>
+            <div className="animals">
+                <img className="Sil" src = {SilDog}/>
+                <img className="Sil" src = {SilMouse}/>
+                <img className="Sil" src = {SilRep}/>
+                <img className="Sil" src = {SilHabit}/>
+                <img className="Sil" src = {SilFish}/>
+                <img className="Sil" src = {SilBird}/>
+                <img className="Sil" src = {SilCat}/>
+                <br />
+                <img className="Sil2" src = {HomeText}/>
+                <br />
+                <h5 className="destaque">Ajudando pequenas empresas e produtores a dar o suporte que todo tutor de pet precisa nessa incrivel jornada!</h5>
+                <br />
+                <br />
+                
 
+            </div>
+           <Carousel/>
+            <br/>
+            <h1 className="destaque">Podutos em Destaque</h1>
+            
+            
            <ProductList/>
         </>
     )
