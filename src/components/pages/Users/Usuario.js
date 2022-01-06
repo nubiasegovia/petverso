d569d054c367505325ea005eaf84a441079c1842
amorajuno <amorajuno@gmail.com>
2022-01-06 12:46:23 -0300

    add useNavigate no cadastro de usuário

--- src/components/pages/Users/Usuario.js
+++ src/components/pages/Users/Usuario.js
@@ -1,31 +1,27 @@
-import React from 'react';
-import './Usuario.css';
-import Cadastro from '../../assets/cadastro.png';
+import "./Usuario.css";
+import Cadastro from "../../assets/cadastro.png";
 // eslint-disable-next-line
-import axios from 'axios';
-import { useState } from 'react';
+import axios from "axios";
+import { useState, React } from "react";
+import { useNavigate } from 'react-router-dom'
 
-
-
-const CadastroUser = () => {
+const CadastroUser = (props) => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [birthday, setBirthday] = useState("");
+    const [cep, setCep] = useState("");
+    const [cpf, setCpf] = useState("");
+    const [phone, setPhone] = useState("");
+    const [city, setCity] = useState("");
+    const [state, setState] = useState("");
+    const [address1, setAddress1] = useState("");
+    const [address2, setAddress2] = useState("");
     
-
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [passwordConfirmation, setPasswordConfirmation] = useState('')
-    const [birthday, setBirthday] = useState('')
-    const [cep, setCep] = useState('')
-    const [cpf, setCpf] = useState('')
-    const [phone, setPhone] = useState('')
-    const [city, setCity] = useState('')
-    const [state, setState] = useState('')
-    const [address1, setAddress1] = useState('')
-    const [address2, setAddress2] = useState('')
-   
-
-    const handleSubmit = async (event) =>{
+    const navigate = useNavigate();
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         // eslint-disable-next-line
@@ -42,84 +38,184 @@ const CadastroUser = () => {
             address2: address2,
             city: city,
             state: state,
-            birthday:birthday
-        }
+            birthday: birthday,
+        };
 
-        const res = await axios.post('/user/registrar', data)
-            .then(res => {
-                console.log(res);
-                console.log(res.data);
-              })
-    }
 
+        
+        const response = await axios.post("/user/registrar", data).then((res) => {
+            console.log(res);
+            console.log(response.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        navigate("/products/all");
+    };
+    
     return (
         <>
             <div class="registration-form">
                 <form onSubmit={handleSubmit}>
-                    <div className="form-group"> 
-                        <img className="imgcad" alt="" src= { Cadastro } />
+                    <div className="form-group">
+                        <img className="imgcad" alt="" src={Cadastro} />
                     </div>
                     <div class="form-group">
-                        <input type="text" require onChange={event => setName(event.target.value)}  class="form-control item" id="name" placeholder="Nome Completo"/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setName(event.target.value)}
+                            class="form-control item"
+                            id="name"
+                            placeholder="Nome Completo"
+                        />
                     </div>
                     <div class="form-group">
-                        <input type="text" require onChange={event => setUsername(event.target.value)} class="form-control item" id="username" placeholder="Usuário"/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setUsername(event.target.value)}
+                            class="form-control item"
+                            id="username"
+                            placeholder="Usuário"
+                        />
                     </div>
                     <div class="form-group">
-                        <input type="password" require onChange={event => setPassword(event.target.value)} class="form-control item" id="password" placeholder="Senha"/>
-                    </div>  
-                    <div class="form-group">
-                        <input type="password" require onChange={event => setPasswordConfirmation(event.target.value)} class="form-control item" id="passwordConfirmation" placeholder="Confirmação de Senha"/>
-                    </div>                  
-                    <div class="form-group">
-                        <input type="text" require onChange={event => setEmail(event.target.value)} class="form-control item" id="email" placeholder="E-mail"/>
+                        <input
+                            type="password"
+                            require
+                            onChange={(event) => setPassword(event.target.value)}
+                            class="form-control item"
+                            id="password"
+                            placeholder="Senha"
+                        />
                     </div>
                     <div class="form-group">
-                        <input type="text" require onChange={event => setPhone(event.target.value)} class="form-control item" id="phone" placeholder="Nº de Telefone/Whatsapp"/>
+                        <input
+                            type="password"
+                            require
+                            onChange={(event) => setPasswordConfirmation(event.target.value)}
+                            class="form-control item"
+                            id="passwordConfirmation"
+                            placeholder="Confirmação de Senha"
+                        />
                     </div>
-                    
                     <div class="form-group">
-                        <input type="text"  require onChange={event => setBirthday(event.target.value)}class="form-control item" id="birth-date" placeholder="Data de Nascimento"/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setEmail(event.target.value)}
+                            class="form-control item"
+                            id="email"
+                            placeholder="E-mail"
+                        />
                     </div>
                     <div class="form-group">
-                        <input type="text" require onChange={event => setCpf(event.target.value)} class="form-control item" id="CPF" placeholder="CPF"/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setPhone(event.target.value)}
+                            class="form-control item"
+                            id="phone"
+                            placeholder="Nº de Telefone/Whatsapp"
+                        />
+                    </div>
+
+                    <div class="form-group">
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setBirthday(event.target.value)}
+                            class="form-control item"
+                            id="birth-date"
+                            placeholder="Data de Nascimento"
+                        />
+                    </div>
+                    <div class="form-group">
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setCpf(event.target.value)}
+                            class="form-control item"
+                            id="CPF"
+                            placeholder="CPF"
+                        />
                     </div>
                     <div class="form-group">
                         <label for="inputAddress">Endereço</label>
-                        <input type="text" require onChange={event => setAddress1(event.target.value)} class="form-control" id="address1" placeholder="Dog Street, nº 0"/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setAddress1(event.target.value)}
+                            class="form-control"
+                            id="address1"
+                            placeholder="Dog Street, nº 0"
+                        />
                     </div>
                     <div class="form-group">
                         <label for="inputAddress2">Complemento</label>
-                        <input type="text" require onChange={event => setAddress2(event.target.value)} class="form-control" id="address2" placeholder="Casa, Apto, etc."/>
+                        <input
+                            type="text"
+                            require
+                            onChange={(event) => setAddress2(event.target.value)}
+                            class="form-control"
+                            id="address2"
+                            placeholder="Casa, Apto, etc."
+                        />
                     </div>
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                        <label for="inputCity">Cidade</label>
-                        <input type="text"  require onChange={event => setCity(event.target.value)}class="form-control" id="inputCity"/>
+                            <label for="inputCity">Cidade</label>
+                            <input
+                                type="text"
+                                require
+                                onChange={(event) => setCity(event.target.value)}
+                                class="form-control"
+                                id="inputCity"
+                            />
                         </div>
                         <div class="form-group col-md-6">
-                        <label for="inputCity">Estado</label>
-                        <input type="text" require onChange={event => setState(event.target.value)}class="form-control" id="inputCity"/>
+                            <label for="inputCity">Estado</label>
+                            <input
+                                type="text"
+                                require
+                                onChange={(event) => setState(event.target.value)}
+                                class="form-control"
+                                id="inputCity"
+                            />
                         </div>
                         <div class="form-group col-md-4">
-                        <label for="inputCEP">CEP</label>
-                        <input type="text" require onChange={event => setCep(event.target.value)}class="form-control" id="inputCEP"/>
+                            <label for="inputCEP">CEP</label>
+                            <input
+                                type="text"
+                                require
+                                onChange={(event) => setCep(event.target.value)}
+                                class="form-control"
+                                id="inputCEP"
+                            />
                         </div>
                     </div>
                     <div class="form-group">
-                        <button type="submit" value='Enviar' class="btn btn-block create-account">Criar Conta</button>
+                        <button
+                            type="submit"
+                            value="Enviar"
+                            class="btn btn-block create-account"
+                        >
+                            Criar Conta
+                        </button> 
                     </div>
                 </form>
                 <div class="social-media">
                     <h5>Cadastre-se com sua mídia social</h5>
                     <div class="social-icons">
-                            <a href="/"><i class="icon-social-google" title="Google"></i></a>
+                        <a href="/">
+                            <i class="icon-social-google" title="Google"></i>
+                        </a>
                     </div>
                 </div>
             </div>
-                
         </>
-    )
-}
+    );
+};
 
 export default CadastroUser;
